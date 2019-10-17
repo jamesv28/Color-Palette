a92fe7b5f72d7f49c2d3fe61ddee89886b64d644
James Volmert <james.volmert@gmail.com>
2019-10-17 16:34:04 -0600

    syled color boxes

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -2,10 +2,16 @@ import React, { Component } from 'react';
 
 class ColorBox extends Component {
     render() {
+        const {name, background} = this.props;
         return (
-            <div style={{background: this.props.background}} className="colorBox">
-                <span>{this.props.name}</span>
-                <span>More</span>
+            <div style={{background: background}} className="colorBox">
+                <div className="copy-container">
+                    <div className="body-content">
+                        <span>{name}</span>
+                        <button className="see-more">More</button>
+                    </div>
+                    <button className="copy-button">Copy</button>
+                </div>
             </div>
         )
     }
