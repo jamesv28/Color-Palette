fdbcb4f74b9b14d86d700570384cda61729c4f5c
James Volmert <james.volmert@gmail.com>
2019-10-17 13:00:02 -0600

    added palette color

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Palette from './Palette';
+import seedColors from './seedColors';
 
 function App() {
   return (
     <div >
-      
+      <Palette {...seedColors[4]} />
     </div>
   );
 }
