2d654e42d1c5bbf0d5f6c8e44eeb90e93294d3e7
James Volmert <james.volmert@gmail.com>
2019-10-17 16:52:50 -0600

    styled more and added hover effect

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
+import {CopyToClipboard} from 'react-copy-to-clipboard';
 
 class ColorBox extends Component {
+
+    constructor(props) {
+        super(props);
+    }
     render() {
         const {name, background} = this.props;
         return (
-            <div style={{background: background}} className="colorBox">
-                <div className="copy-container">
-                    <div className="body-content">
-                        <span>{name}</span>
-                        <button className="see-more">More</button>
+            <CopyToClipboard text={background}>
+                <div style={{background: background}} className="colorBox hvr-pulse-grow">
+                    <div className="copy-container">
+                        <div className="body-content">
+                            <span>{name}</span>
+                            <button className="see-more ">More</button>
+                        </div>
+                        <button className="copy-button">Copy</button>
                     </div>
-                    <button className="copy-button">Copy</button>
                 </div>
-            </div>
+            </CopyToClipboard>
         )
     }
 }
