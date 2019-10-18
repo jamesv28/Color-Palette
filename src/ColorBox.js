312a7bc81146295ccb31f0d25e009ca3d3e3a32a
James Volmert <james.volmert@gmail.com>
2019-10-18 15:37:42 -0600

    fixed color animation when copying to clipboard

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -5,12 +5,32 @@ class ColorBox extends Component {
 
     constructor(props) {
         super(props);
+        this.state = {
+            copied: false
+        }
+        this.changeCopyState = this.changeCopyState.bind(this);
     }
+
+    changeCopyState() {
+        this.setState({
+            copied: true
+        }, () => {
+            setTimeout( () => this.setState({copied: false}), 1500)
+        })
+    }
+
     render() {
         const {name, background} = this.props;
+        const {copied} = this.state;
+
         return (
-            <CopyToClipboard text={background}>
-                <div style={{background: background}} className="colorBox hvr-pulse-grow">
+            <CopyToClipboard text={background} onCopy={this.changeCopyState} >
+                <div style={{background: background}} className="colorBox">
+                    <div style={{ background}} className={`color-overlay ${copied ? 'show' : ''}`}/>
+                    <div className={`copy-msg ${copied ? 'show' : ''}`}>
+                        <h1 >Copied!!!</h1>
+                        <p>{this.props.background}</p>
+                    </div>
                     <div className="copy-container">
                         <div className="body-content">
                             <span>{name}</span>
