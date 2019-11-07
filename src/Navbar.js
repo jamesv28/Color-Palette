ec907765af4df3a9bd7ce16774da6ade113ab8a1
James Volmert <james.volmert@gmail.com>
2019-11-06 19:19:35 -0700

    added snackbar and created footer

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,28 +3,45 @@ import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css';
 import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
+import Snackbar from '@material-ui/core/Snackbar';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
 
 class Navbar extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            format: 'hex'
+            format: 'hex',
+            open: false
         }
         this.handleChange = this.handleChange.bind(this);
+        this.closeSnackbar = this.closeSnackbar.bind(this);
+        this.capWord = this.capWord.bind(this);
     }
 
     handleChange(e) {
         this.setState({
-            format: e.target.value
+            format: e.target.value,
+            open: true
         });
         this.props.handleChange(e.target.value);
+    }
+
+    closeSnackbar() {
+        this.setState({
+            open: false
+        })
+    }
+
+    capWord(word) {
+        let res = word.toUpperCase;
+        return res;
     }
 
     render() {
         const {
                 level, 
-                changeLevel,
-                handleChange
+                changeLevel
             } = this.props;
         const {format} = this.state;
 
@@ -55,6 +72,25 @@ class Navbar extends Component {
                             <MenuItem value="rgba">RGBA - rgba(255,255,255,1)</MenuItem>
                         </Select>
                 </div>
+                <Snackbar 
+                    anchorOrigin={{vertical: 'bottom', horizontal: 'left' }}
+                    open={this.state.open}
+                    autoHideDuration={3000}
+                    message={<span>Format Changed: {format}</span>}
+                    onClose={this.closeSnackbar}
+                    action={
+                        [
+                            <IconButton 
+                                color="inherit"
+                                onClick={this.closeSnackbar}
+                                key="Close"
+                                aria-label="Close"
+                            >
+                                <CloseIcon />
+                            </IconButton>
+                        ]
+                    }
+                />
             </header>
         )
     }
--- src/Palette.js
+++ src/Palette.js
@@ -27,10 +27,10 @@ class Palette extends Component {
     }
 
     render() {
-        const {colors} = this.props.palette;
+        const {colors, paletteName, emoji} = this.props.palette;
         const {level, format} = this.state;
         const colorBoxes = colors[level].map(color => (
-            <ColorBox background={color[format]} name={color.name} />
+            <ColorBox background={color[format]} name={color.name} key={color.id} />
         ))
         return (
             <div className="palette">
@@ -42,7 +42,12 @@ class Palette extends Component {
                 <div className="palette-colors">
                     {colorBoxes}
                 </div>
-
+                <footer className="palette-footer">
+                    {paletteName}
+                    <span className="emoji">
+                        {emoji}
+                    </span>
+                </footer>
             </div>
         )
     }
